539fc80511ca585e632903ae5b35934a37e444e9
apptain <wontcheck@apptain.com>
2022-05-01 13:46:11 -0400

    removes layout and updates config manifest icon path

--- config/gatsby-config.ts
+++ config/gatsby-config.ts
@@ -158,7 +158,7 @@ export default {
         short_name: config.title,
         theme_color: "hsl(31, 92%, 62%)",
         background_color: "hsl(0, 0%, 100%)",
-        icon: "content/anydub.png",
+        icon: 'static/media/favico.png',
         display: "standalone",
         start_url: "/",
       },
